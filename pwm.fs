442a1d3fb41713a87d2069fd0071c62b9c2b9451
Jim Morris <morris@wolfman.com>
2025-02-03 13:26:31 +0000

    Change pwm base period to 1us (1MHz) so we can use 2KHz and go down to 1% duty cycle

--- pwm.fs
+++ pwm.fs
@@ -1,11 +1,12 @@
 \ pwm driver
 
-0 variable PWM_PERIOD_MS
+0 variable PWM_PERIOD_US
 
-: pwm-set-pwm ( periodms dutycylems -- )
-	1- TIM3 _tCCR1 !	\ duty cycle
-	dup 1- TIM3 _tARR !	\ period
-	PWM_PERIOD_MS !
+: pwm-set-pwm_us ( periodus dutycyleus -- )
+	\ base frequency is 1us
+	1- TIM3 _tCCR1 !		\ duty cycle
+	dup 1- TIM3 _tARR !		\ period
+	PWM_PERIOD_US !
 ;
 
 : pwm-init ( -- )
@@ -18,8 +19,8 @@
 	\ --- init timer 3
     1 1 lshift RCC _rAPB1ENR bis!			\ TIM3 clock enabled
 	1 7 lshift TIM3 _tCR1 ! 				\ Auto-reload preload enable ARPE
-	HCLK @ 2000 / 1- TIM3 _tPSC !			\ 42Mhz hclk prescaler -> 1000 Hz = 1 ms
-	100 50 pwm-set-pwm						\ 100ms - 50ms period - dutycycle as initial value
+	HCLK @ 1000000 / 1- TIM3 _tPSC !		\ 84Mhz hclk prescaler -> 1000000 Hz = 1us
+	1000 500 pwm-set-pwm_us					\ 1ms - 500us period - dutycycle as initial value
 	1 0 lshift TIM3 _tCCER !				\ CC1E -> OC1 signal is output on the corresponding output pin
 	1 3 lshift TIM3 _tCCMR1 bis!			\ OC1PE -> Output compare 1 preload enable
 	%110 4 lshift TIM3 _tCCMR1 bis!			\ Output compare 1 mode -> 110: PWM mode 1
@@ -32,23 +33,24 @@
 	rot if bis! else bic! then
 ;
 
-: pwm-pulse_width ( pwms -- )
+: pwm-pulse_width ( pwus -- )
 	1- TIM3 _tCCR1 !
 ;
 
 \ calculate the pulse width based on the percentage duty cycle
 : pwm-duty_cycle ( dc% -- )
-	100 PWM_PERIOD_MS @ -rot u*/ pwm-pulse_width
+	100 PWM_PERIOD_US @ -rot u*/ pwm-pulse_width
 ;
 
 \ calculate the pulse width based on 0 - 255
 : pwm-duty_cycle_255 ( dc -- )
-	255 PWM_PERIOD_MS @ -rot u*/ pwm-pulse_width
+	255 PWM_PERIOD_US @ -rot u*/ pwm-pulse_width
 ;
 
 : test-pwm ( dc -- )
 	pwm-init
 	pwm-duty_cycle
+	\ pwm-set-pwm_us
 	true pwm-enable
 	begin key? until
 	false pwm-enable
