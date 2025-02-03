c2a9a6213cc0c0d34a54fa5e6b201916a3ad53aa
Jim Morris <morris@wolfman.com>
2025-02-03 01:40:36 +0000

    add spi1 and psram to test

---
+++ psram.fs
@@ -0,0 +1,58 @@
+\ psram driver SPI
+
+\ #include spi1.fs
+\ #include gpio-simple.fs
+
+PORTB 6 pin PSRAM_CS
+
+: psram-write ( n buf addr -- errflg )
+	0 PSRAM_CS set
+	$02 spi!
+	dup 16 rshift $FF and spi!
+	dup  8 rshift $FF and spi!
+	$FF and spi!
+	swap 0 do dup i + c@ spi! loop
+	drop
+	1 PSRAM_CS set
+	false
+;
+
+: psram-read ( n buf addr -- errflg )
+	0 PSRAM_CS set
+	$02 spi!
+	dup 16 rshift $FF and spi!
+	dup  8 rshift $FF and spi!
+	$FF and spi!
+	swap 0 do dup i + spi-read1 swap c! loop
+	drop
+	1 PSRAM_CS set
+	false
+;
+
+10 buffer: psram_buf
+: test-psram
+	PSRAM_CS output
+	1 PSRAM_CS set
+	\ SPISettings(4000000, MSBFIRST, SPI_MODE0));
+	spi-init
+
+	0 PSRAM_CS set
+	$9F spi!
+	\ dummy address
+	$00 spi!
+	$00 spi!
+	$00 spi!
+
+	\ read ID
+	spi-read1
+	spi-read1
+	spi-read1
+
+	1 PSRAM_CS set
+
+	." jedec id= " hex. hex. hex. cr
+
+	s" 123456789" swap 0 psram-write drop
+	9 psram_buf 0 psram-read drop
+	psram_buf 9 type
+;
---
+++ spi1.fs
@@ -0,0 +1,99 @@
+\ SPI1 PB3 - SCK, PB4 - MISO, PB5 - MOSI at AF05
+
+\ helpers
+
+\ sets or clears a range of bits in addr
+: bits! ( value mask pos addr -- )
+    >r tuck         \ -- value pos mask pos
+    lshift r@ bic!  \ clear mask first
+    lshift r> bis!  \ set the value bits
+;
+
+$40013000 constant SPI1
+
+\ cpol cpha
+$00 constant SPI_MODE0
+$01 constant SPI_MODE1
+$10 constant SPI_MODE2
+$11 constant SPI_MODE3
+
+: spi-init
+    \ Enable SPI1 clock (on APB2)
+    1 12 lshift RCC _rAPB2ENR   \ Set SPI1EN bit
+    \ Enable GPIOA clock
+    1 RCC _rAHB1ENR             \ Enable GPIOA clock
+
+    \ Configure PB3 (SCK), PB4 (MISO), PB5 (MOSI)
+    MODE_Alternate 3 PORTA set-moder
+    MODE_Alternate 4 PORTA set-moder
+    MODE_Alternate 5 PORTA set-moder
+
+    \ Select AF5 (SPI1) for these pins
+    5 3 PORTA set-alternate
+    5 4 PORTA set-alternate
+    5 5 PORTA set-alternate
+
+    \ Set GPIO pins to High-speed mode
+    SPEED_HIGH 3 PORTA set-opspeed
+    SPEED_HIGH 4 PORTA set-opspeed
+    SPEED_HIGH 5 PORTA set-opspeed
+
+    \ Set push-pull mode and no pull-up/pull-down
+    1 3 lshift 1 4 lshift or 1 5 lshift or PORTA _pOTYPER bic!
+    1 3 lshift 1 4 lshift or 1 5 lshift or PORTA _pPUPDR bic!
+
+    \ Disable SPI before configuring and set everything to 0
+    0 SPI1 _sCR1 !
+    \ Set Master mode
+    1 2 lshift SPI1 _sCR1 bis!
+    \ Set Baud rate (fPCLK/32) -> BR[2:0] = 0b100 @ 84MHz = 2.625MHz
+    %100 3 lshift SPI1 _sCR1 bis!
+    \ Set Clock Polarity (CPOL = 0) and Clock Phase (CPHA = 0)
+    %11 SPI1 _sCR1 bic!
+    \ Set Data Frame Format to 8-bit (DFF = 0)
+    1 11 lshift SPI1 _sCR1 bic!
+    \ Set MSB First (LSBFIRST = 0)
+    1 7 lshift SPI1 _sCR1 bic!
+
+    \ Set Software Slave Management (SSM = 1), Internal Slave Select (SSI = 1)
+    1 9 lshift 1 8 lshift or SPI1 _sCR1 bis!
+    \ Enable SPI Peripheral
+    1 6 lshift SPI1 _sCR1 bis!
+;
+
+\ find the closest baud rate that is slower or equal to the requested one
+: spi-baud ( baud -- )
+    8 0 do
+        hclk @ 1 i 1+ lshift / over
+        u<= if
+            i %111 3 SPI1 _sCR1 bits!
+            unloop drop exit
+        then
+    loop
+    \ use slowest
+    %111 %111 3 SPI1 _sCR1 bits!
+    drop
+;
+
+: spi-busy? ( -- flg )
+    %10000000 SPI1 _sSR bit@        \ check Busy
+;
+
+: spi-enable ( flg -- )
+    %1000000 SPI1 _sCR1 rot if bis! else bic! then
+;
+
+: spi! ( data -- )
+    begin %10 SPI1 _sSR bit@ until \ Wait for TXE
+    SPI1 _sDR                      \ Send data
+;
+
+: spi@ ( -- data )
+    begin %1 SPI1 _sSR bit@ until   \ Wait for RXNE
+    SPI1 _sDR                       \ Read received data
+;
+
+: spi-read1 ( -- data)
+    0 spi!
+    spi@
+;
