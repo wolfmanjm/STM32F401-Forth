3e98e4f4ae7171485edd763815bc94aad8e2213c
Jim Morris <morris@wolfman.com>
2025-02-04 17:01:42 +0000

    try to get memtest to work

--- psram.fs
+++ psram.fs
@@ -3,6 +3,9 @@
 \ #include spi1.fs
 \ #include gpio-simple.fs
 
+\ 1024 8 * 1024 * constant PSRAM_MEMSIZE \ 8MB ram size
+1024 1024 * 2 * constant PSRAM_MEMSIZE \ 8MB ram size
+
 PORTB 6 pin PSRAM_CS
 
 : psram-cs ( flg -- )
@@ -37,12 +40,32 @@ PORTB 6 pin PSRAM_CS
 	false
 ;
 
+: psram-dump ( n addr -- )
+	cr
+	true psram-cs
+	$03 psram-cmd-addr
+	1+ 1 do
+		spi-read1 hex.2 space
+		i 16 mod 0= if cr then
+	loop
+	false psram-cs
+	cr
+;
+
+: REVERSE ( i*x i -- i*y ) 0 DO I ROLL LOOP ;
+: apply-top ( i*x u.i xt -- j*x ) \ xt ( x -- k*x )
+  swap dup 0= if 2drop exit then ( ... x xt u.i )
+  rot >r 1- swap dup >r recurse ( R: x xt )
+  2r> execute
+;
+
 10 buffer: psram_buf
 : test-psram
 	PSRAM_CS output
 	false psram-cs
 	\ SPISettings(4000000, MSBFIRST, SPI_MODE0));
 	spi-init
+	4000000 spi-baud
 
 	true psram-cs
 	0 $9F psram-cmd-addr
@@ -63,7 +86,7 @@ PORTB 6 pin PSRAM_CS
 	." expect 1 2 3 4 5 6 using >spi and spi>" cr
 	\ write data from the stack
 	true psram-cs
-	$10 $02 psram-cmd-addr 6 5 4 3 2 1 6 >spi
+	$10 $02 psram-cmd-addr 6 5 4 3 2 1 depth >spi
 	spi-waitbusy
 	false psram-cs
 
@@ -72,5 +95,92 @@ PORTB 6 pin PSRAM_CS
 	$10 $03 psram-cmd-addr 6 spi>
 	spi-waitbusy
 	false psram-cs
-	6 0 do . loop cr
+	depth reverse depth 0 do . loop cr
+	\ 6 ['] . apply-top cr
+;
+
+\ pseudo random
+7 variable seed
+: random    ( -- x )  \ return a 32-bit random number x
+    seed @
+    dup 13 lshift xor
+    dup 17 rshift xor
+    dup 5  lshift xor
+    dup seed !
+;
+
+: setseed   ( x -- )  \ seed the RNG with x
+    dup 0= or         \ map 0 to -1
+    seed !
+;
+
+: psram-memtestwr ( seed -- )
+	cr ." writing "
+	setseed
+
+	true psram-cs
+	0 $02 psram-cmd-addr
+	PSRAM_MEMSIZE 4 / 4 - 1 do
+		random
+		dup           $FF and spi-write1
+		dup 8  rshift $FF and spi-write1
+		dup 16 rshift $FF and spi-write1
+			24 rshift $FF and spi-write1
+
+		\ page boundary is 1024
+		i 252 mod 0= if
+			spi-waitbusy
+			false psram-cs
+			true psram-cs
+			i 4 + 4 * $02 psram-cmd-addr
+		then
+		i 2048 mod 0= if ." ." then
+	loop
+	spi-waitbusy
+	false psram-cs
+;
+
+0 variable memerr
+: psram-memtestrd ( seed -- )
+	cr
+	." reading "
+	setseed
+
+	true psram-cs
+	0 $03 psram-cmd-addr
+	PSRAM_MEMSIZE 4 / 4 - 1 do
+		i memerr !
+		random
+		spi-read1 over 	 		 $FF and <> if leave then
+		spi-read1 over 8  rshift $FF and <> if leave then
+		spi-read1 over 16 rshift $FF and <> if leave then
+		spi-read1 swap 24 rshift $FF and <> if leave then
+
+		\ page boundary is 1024
+		i 252 mod 0= if
+			spi-waitbusy
+			false psram-cs
+			true psram-cs
+			i 4 + 4 * $03 psram-cmd-addr
+		then
+		i 2048 mod 0= if ." ." then
+		0 memerr !
+	loop
+	memerr @ 0<> if ." memory error @ " memerr @ . then
+	spi-waitbusy
+	false psram-cs
+;
+
+: memtest
+	." testing " PSRAM_MEMSIZE 1024 / . ." KBytes" cr
+	PSRAM_CS output
+	false psram-cs
+	spi-init
+	4000000 spi-baud
+	$1357
+	begin
+		dup psram-memtestwr
+		dup psram-memtestrd
+		$1234 +
+	key? until
 ;
